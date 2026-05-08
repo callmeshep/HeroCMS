8c370c2aab7a3b1ce53d704adc76bd719bc4ab4d
Andy Pink <andy@tradeclicks.co.uk>
2026-05-08 19:21:51 +0100

    fresh migration for all collections

---
+++ hero-cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260508_181839 from './20260508_181839';
+
+export const migrations = [
+  {
+    up: migration_20260508_181839.up,
+    down: migration_20260508_181839.down,
+    name: '20260508_181839'
+  },
+];
