e31a48768ab70088d8ae67306f6be743d525c0f2
Andy Pink <andy@tradeclicks.co.uk>
2026-05-10 17:00:43 +0100

    inline Emergency Hero SVGs for admin branding

--- hero-cms/src/graphics/Icon/index.tsx
+++ hero-cms/src/graphics/Icon/index.tsx
@@ -1,9 +1,79 @@
-export default function Icon() {
+import React from 'react'
+
+export const Icon: React.FC = () => {
   return (
-    <img
-      src="/api/emergency-hero-brand-assets/file/Asset%204.svg"
-      alt="Emergency Hero"
-      style={{ height: '28px', width: 'auto' }}
-    />
+    <svg
+      viewBox="0 0 347.7 437.43"
+      style={{ width: '28px', height: 'auto' }}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <linearGradient
+          id="icon-linear-gradient"
+          x1="141.38"
+          y1="428.75"
+          x2="349.52"
+          y2="220.61"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset=".02" stopColor="#007bc9" />
+          <stop offset=".5" stopColor="#00a3f3" />
+          <stop offset="1" stopColor="#007dcb" />
+        </linearGradient>
+        <linearGradient
+          id="icon-linear-gradient-2"
+          x1="3.62"
+          y1="261.89"
+          x2="219.68"
+          y2="45.83"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stopColor="#00920d" />
+          <stop offset=".5" stopColor="#22b82d" />
+          <stop offset="1" stopColor="#00920d" />
+        </linearGradient>
+        <linearGradient
+          id="icon-linear-gradient-3"
+          x1="187.69"
+          y1="215.85"
+          x2="288.26"
+          y2="96"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset=".26" stopColor="#00abfb" />
+          <stop offset="1" stopColor="#88deed" />
+        </linearGradient>
+        <linearGradient
+          id="icon-linear-gradient-4"
+          x1="157.52"
+          y1="261.53"
+          x2="41.74"
+          y2="377.31"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset=".26" stopColor="#00ab00" />
+          <stop offset="1" stopColor="#88de60" />
+        </linearGradient>
+      </defs>
+      <g>
+        <path
+          fill="url(#icon-linear-gradient)"
+          d="M329.14,200.22s-53.86-18.42-79.35,6.96c29.93,0,16.52,21.52,16.52,21.52,0,0-97.14,124.55-114.88,146.93-8.95,11.3-15.42,3.51-13.08-3.62-14.21,16.12-19.63,59.82,27.8,65.25,2.56.12,5.13.17,7.71.17,96.01,0,173.85-77.84,173.85-173.85,0-1.97-.03-3.92-.1-5.87-.68-20.24-11.45-39.57-18.47-57.48Z"
+        />
+        <path
+          fill="url(#icon-linear-gradient-2)"
+          d="M173.85,0C100.11,73.73,20.51,148.41,3.37,231.29c-2.2,10.62,2.35,29.25,8.33,38.37,8.41,12.85,65.43,25,79.83-6.68-29.13,0-17.46-20.37-17.46-20.37,0,0,105.32-139.03,112.76-148.78,7.45-9.75,13.55-3.26,11.68,2.05,35.17-14.55-2.69-69.23-24.66-95.87Z"
+        />
+        <path
+          fill="url(#icon-linear-gradient-3)"
+          d="M170.42,192.21c-2.33,7.51,3.45,14.97,11.31,14.97h107.09c24.49,0,54.21,14.81,58.7,50.52C343.93,164.12,255.33,81.48,173.85,0c16.37,23.37,36,55.15,24.66,95.87-13.36,48-22.85,79.48-28.09,96.34Z"
+        />
+        <path
+          fill="url(#icon-linear-gradient-4)"
+          d="M165.46,278.46c2.27-7.7-3.37-15.48-11.39-15.48H31.91c-19.31,0-28.74-11.08-28.49-31.69-2.2,10.63-3.42,21.38-3.42,32.28,0,93.43,73.71,169.62,166.14,173.66-22.43-7.04-36.67-34.17-27.8-65.23,11.92-41.73,21.71-75.18,27.12-93.55Z"
+        />
+      </g>
+    </svg>
   )
 }
