1b4f9213c476446134bc4ad1a85f124ddc4b549d
Andy Pink <andy@tradeclicks.co.uk>
2026-05-14 12:06:00 +0100

    Add migration for API key columns

--- hero-cms/src/migrations/index.ts
+++ hero-cms/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260508_182823 from './20260508_182823';
 import * as migration_20260510_214014 from './20260510_214014';
+import * as migration_20260514_110559 from './20260514_110559';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260510_214014.up,
     down: migration_20260510_214014.down,
-    name: '20260510_214014'
+    name: '20260510_214014',
+  },
+  {
+    up: migration_20260514_110559.up,
+    down: migration_20260514_110559.down,
+    name: '20260514_110559'
   },
 ];
